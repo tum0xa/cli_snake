dc188fb3eae8f55965ce8eb40e8a0c8604692ea8
tum0xa <mail@tum0xa.ru>
2018-08-15 16:55:49 +0500

    Added collision body  with head

--- cli_snake.py
+++ cli_snake.py
@@ -4,98 +4,93 @@ import random
 
 import keyboard
 
-clear = lambda: os.system('clear')
-
+global x, y, direction, state, fx,fy, snake
 
 gameField_width = 40
 gameField_height = 20
-global x, y, direction, state, fx,fy
+
+DEBUG = True
+
+# Directions
 
 LEFT = 0
 RIGHT = 1
 UP = 2
 DOWN = 3
 
+############
+
+# Game state
+
 PLAY = 0
 END = 1
 
-x = 4
-y = 1
-direction = RIGHT
-state = PLAY
+############
 
-fx = 20
-fy = 10
 
-points = [[3,2],[2,1],[1,1]]
+if os.name == 'posix':
+    clear = lambda: os.system('clear')
+elif os.name == 'nt':
+    clear = lambda: os.system('cls')
+    
+def game_init():
+    global x, y, direction, state, fx,fy, snake
+    
+    x = 4
+    y = 1
+    
+    direction = RIGHT
+    state = PLAY
+
+    fruit()
+    
+    snake = [[3,2],[2,1],[1,1]]
+    
 
 
 def check_point(pointx,pointy):
-    global points
+    global snake
     point = [pointx,pointy]
-    if point in points:
+    if point in snake:
         return True
     else:
         return False
         
-def snake_move(direction):
-    global points, x, y, state
+def check_colision():
+    global snake,state,head
     
-    head = points[0].copy()
+    for point in snake:
+        if snake.count(head) > 1:
+            state = END
+        
+        
+     
+        
+def snake_move(direction):
+    global snake, x, y, state,head
+    
+    head = snake[0].copy()
+
     if direction == RIGHT:
-        head[0] = head[0]+1
+        head[0] = head[0] + 1
     elif direction == LEFT:
-        head[0] = head[0]-1
+        head[0] = head[0] - 1
     elif direction == UP:
-        head[1] = head[1]-1
+        head[1] = head[1] - 1
     elif direction == DOWN:
-        head[1] = head[1]+1
-    points.insert(0,head)
-    points.pop()
+        head[1] = head[1] + 1
+
+    snake.insert(0,head)
+    snake.pop()
     x = head[0]
     y = head[1]
-    if x > gameField_width-2:
-        x = gameField_width-2
-        state = END
-    elif x < 1:
-        x = 1
-        state = END
-    if y > gameField_height-2:
-        y = gameField_height-2
-        state = END
-    elif y < 1:
-        y = 1
-        state = END
+
 
 def snake_eat():
-    global points,fx,fy
-    points.insert(0,[fx,fy])
+    global snake,fx,fy
+    snake.insert(0,[fx,fy])
 
-def move(direction=RIGHT):
-    global x,y, state
-    if direction == RIGHT:
-        x+=1
-    elif direction == LEFT:
-        x-=1
-    elif direction == DOWN:
-        y+=1
-    elif direction == UP:
-        y-=1
-    
-    if x > gameField_width-2:
-        x = gameField_width-2
-        state = END
-    elif x < 1:
-        x = 1
-        state = END
-    if y > gameField_height-2:
-        y = gameField_height-2
-        state = END
-    elif y < 1:
-        y = 1
-        state = END
-
-def draw():
+def draw(message):
     global x,y, state,fx, fy,head
     for i in range(gameField_height):
         for j in range(gameField_width):
@@ -116,30 +111,26 @@ def draw():
                 elif check_point(j,i):
                     print('0',end='')
                 elif j==fx and i==fy:
-                    print('F', end='')    
-                              
-                # ~ elif i == y and j == x and x==0:
-                    # ~ print('X', end='')
-                # ~ elif i == y and j == x and x==gameField_width:
-                    # ~ print('X')
-                # ~ elif i == y and j == x and y==0:
-                    # ~ print('X', end='')
-                # ~ elif i == y and j == x and y==gameField_height:
-                    # ~ print('X', end='')
+                    print('F', end='')                 
                 else:
                     print(' ', end='')
-    print('x =', x)
-    print('y =', y)
-    print('fx =', fx)
-    print('fy =', fy)
-    print(points)
-    if state == END:
-        print('Game over!')
+    if message:
+        print(message)
+        
+        
+    if DEBUG == True:
+        print('x =', x)
+        print('y =', y)
+        print('fx =', fx)
+        print('fy =', fy)
+        print(snake)  
+        
 
 def fruit():
     global fx,fy
     fx=random.randint(1,gameField_width-2)
     fy=random.randint(1,gameField_height-2)
+        
         
 def kb_handler():
     global direction
@@ -153,6 +144,9 @@ def kb_handler():
         direction = LEFT
     elif keyboard.is_pressed('Right') and not direction == LEFT:
         direction = RIGHT
+    elif keyboard.is_pressed('Enter'):
+        game_init()
+        
     
 def exit_game():
     exit()
@@ -162,25 +156,43 @@ def main():
    
     global direction,state,x,y,fx,fy
     
+    game_init()
+    old_length=len(snake)
+    
+    message=''
     while True:
-        kb_handler()
+        kb_handler() # Keyboard handler
+        
         if state == PLAY:
-            # ~ move(direction)
             snake_move(direction)
-            
+            check_colision()
+    
+        if x > gameField_width-2:
+            x = gameField_width-2
+            state = END
+        elif x < 1:
+            x = 1
+            state = END
+        if y > gameField_height-2:
+            y = gameField_height-2
+            state = END
+        elif y < 1:
+            y = 1
+            state = END 
+        
+        if state == END:
+            message = 'Game over! Press "Enter" to start new game or "Esc" to exit.'
+           
+        
         if fx == x and fy == y:
             snake_eat()
             fruit()
-        draw()
-        time.sleep(0.2)
-        clear()
-        
-        
-        
-            
 
-    # ~ time.sleep(0.2)
-      
+        clear()
+        draw(message)
+              
+        time.sleep(0.1)   
+
 
 if __name__ == '__main__':
     main()
